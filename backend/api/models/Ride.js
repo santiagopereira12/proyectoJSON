51e45280390b4a80d4199bb3f8503b92186066e4
santiagopereira12 <spereira98@misena.edu.co>
2023-04-21 18:52:19 -0500

    ProyectoJSON

---
+++ backend/api/models/Ride.js
@@ -0,0 +1,47 @@
+/**
+ * Ride.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+    dishes: {
+      type: 'string',
+      required: true,
+      minLength: 5,
+      maxLength: 100,
+    },
+    costs: {
+      type: 'string',
+      required: true,
+      maxLength: 100,
+    },
+    additions: {
+      type: "string",
+    },
+    additionalCosts: {
+      type: 'string',
+    }
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
